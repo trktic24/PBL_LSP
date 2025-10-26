da9c2ae256ad7f87841a55e7e1c5db27dc550fe2
Dimdim666-trk <adiladimaz2@gmail.com>
2025-10-26 13:50:35 +0700

    Feat: Edit TUK navigation

--- resources/views/layouts/tuk-navigation.blade.php
+++ resources/views/layouts/tuk-navigation.blade.php
@@ -1,40 +1,49 @@
 <header class="header">
     <div class="logo">
-        {{-- Pastikan gambar logo ada di public/img/ --}}
         <img src="{{ asset('img/logo-lsp-polines.png') }}" alt="Logo LSP Polines">
     </div>
     <nav class="nav-menu">
         <ul>
-            <li><a href="#" class="nav-link">Home</a></li>
-            <li><a href="{{ url('/alur-sertifikasi') }}" class="nav-link">Jadwal Asesmen</a></li>
+            <li><a href="{{ url('/') }}" class="nav-link">Home</a></li>
+            <li><a href="#" class="nav-link">Jadwal Asesmen</a></li>
             <li><a href="#" class="nav-link">Sertifikasi</a></li>
             
+            {{-- DROPDOWN MENU: INFO --}}
             <li class="dropdown">
                 <a href="#" class="nav-link">Info <span class="arrow-down"></span></a>
                 <ul class="dropdown-content">
-                    {{-- Ganti dengan rute alur sertifikasi --}}
+                    {{-- 1. Alur Proses (Baru) --}}
                     <li><a href="{{ url('/alur-sertifikasi') }}" class="dropdown-link">Alur Proses</a></li> 
+                    
                     <li><a href="#" class="dropdown-link">Daftar Asesor</a></li>
+                    
+                    {{-- 2. TUK (Link ke Daftar TUK) --}}
                     <li><a href="{{ url('/info-tuk') }}" class="dropdown-link">TUK</a></li>
                 </ul>
             </li>
+            
+            {{-- DROPDOWN MENU: PROFIL --}}
             <li class="dropdown">
                 <a href="#" class="nav-link">Profil <span class="arrow-down"></span></a>
                 <ul class="dropdown-content">
+                    {{-- 3. Visi & Misi --}}
                     <li><a href="{{ url('/visimisi') }}" class="dropdown-link">Visi & Misi</a></li>
+                    
+                    {{-- 4. Struktur --}}
                     <li><a href="{{ url('/struktur') }}" class="dropdown-link">Struktur</a></li>
+                    
+                    {{-- 5. Mitra --}}
                     <li><a href="{{ url('/mitra') }}" class="dropdown-link">Mitra</a></li>
                 </ul>
             </li>
         </ul>
     </nav>
     
-    {{-- Mengatasi Error 'Attempt to read property "name" on null' --}}
+    {{-- Tombol Masuk/User Profile --}}
     @guest
         <a href="{{ route('login') }}" class="btn btn-masuk">Masuk</a>
     @endguest
     @auth
-        {{-- Tampilkan nama pengguna jika sudah login, jika ada --}}
-        <a href="{{ url('/profile') }}" class="btn btn-masuk">{{ Auth::user()->name }}</a>
+        <a href="{{ url('/dashboard') }}" class="btn btn-masuk">{{ Auth::user()->name }}</a>
     @endauth
 </header>
