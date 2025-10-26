b8b52fe99d47fd0445e04c8f9f86d5d0b9d700a3
Dimdim666-trk <adiladimaz2@gmail.com>
2025-10-26 14:04:52 +0700

    Feat: Edit TUK navigation

--- resources/views/layouts/tuk-navigation.blade.php
+++ resources/views/layouts/tuk-navigation.blade.php
@@ -1,23 +1,24 @@
 <header class="header">
     <div class="logo">
-        {{-- Pastikan gambar logo ada di public/img/ --}}
         <img src="{{ asset('img/logo-lsp-polines.png') }}" alt="Logo LSP Polines">
     </div>
     <nav class="nav-menu">
         <ul>
-            <li><a href="#" class="nav-link">Home</a></li>
+            <li><a href="{{ url('/') }}" class="nav-link">Home</a></li>
             <li><a href="{{ url('/alur-sertifikasi') }}" class="nav-link">Jadwal Asesmen</a></li>
             <li><a href="#" class="nav-link">Sertifikasi</a></li>
             
+            {{-- DROPDOWN MENU: INFO --}}
             <li class="dropdown">
                 <a href="#" class="nav-link">Info <span class="arrow-down"></span></a>
                 <ul class="dropdown-content">
-                    {{-- Ganti dengan rute alur sertifikasi --}}
                     <li><a href="{{ url('/alur-sertifikasi') }}" class="dropdown-link">Alur Proses</a></li> 
                     <li><a href="#" class="dropdown-link">Daftar Asesor</a></li>
                     <li><a href="{{ url('/info-tuk') }}" class="dropdown-link">TUK</a></li>
                 </ul>
             </li>
+            
+            {{-- DROPDOWN MENU: PROFIL --}}
             <li class="dropdown">
                 <a href="#" class="nav-link">Profil <span class="arrow-down"></span></a>
                 <ul class="dropdown-content">
@@ -29,12 +30,11 @@
         </ul>
     </nav>
     
-    {{-- Mengatasi Error 'Attempt to read property "name" on null' --}}
+    {{-- Tombol Masuk/User Profile (Melindungi dari Error Auth::user()::name) --}}
     @guest
         <a href="{{ route('login') }}" class="btn btn-masuk">Masuk</a>
     @endguest
     @auth
-        {{-- Tampilkan nama pengguna jika sudah login, jika ada --}}
-        <a href="{{ url('/profile') }}" class="btn btn-masuk">{{ Auth::user()->name }}</a>
+        <a href="{{ url('/dashboard') }}" class="btn btn-masuk">{{ Auth::user()->name }}</a>
     @endauth
 </header>
