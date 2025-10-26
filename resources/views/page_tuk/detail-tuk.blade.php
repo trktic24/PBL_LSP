a336837f73ebefbf3596b62a772b5a37d7a3a4d1
Dimdim666-trk <adiladimaz2@gmail.com>
2025-10-26 23:39:43 +0700

    Feat: Styling detail TUK

--- resources/views/page_tuk/detail-tuk.blade.php
+++ resources/views/page_tuk/detail-tuk.blade.php
@@ -21,7 +21,8 @@
     <h1 class="text-3xl font-semibold text-center mb-10 text-gray-800">Tempat Uji Kompetensi</h1>
 
     {{-- KARTU UTAMA: DETAIL TUK CARD --}}
-    <div class="bg-white rounded-xl shadow-xl overflow-hidden border-t-8 border-blue-600 flex flex-col md:flex-row">
+    {{-- MENGHILANGKAN: border-t-8 border-blue-600 --}}
+    <div class="bg-white rounded-xl shadow-xl overflow-hidden flex flex-col md:flex-row">
         
         {{-- BAGIAN KIRI: CARD INFO --}}
         <div class="p-8 lg:p-12 md:w-1/2 space-y-4">
@@ -30,19 +31,20 @@
             
             {{-- Detail Alamat --}}
             <div class="flex items-center space-x-3 text-lg text-gray-700">
-                <i class="fas fa-map-marker-alt w-5 h-5 text-blue-600"></i> {{-- ICON MAPS --}}
+                <i class="fas fa-map-marker-alt w-5 h-5 text-gray-900"></i> {{-- UBAH: text-blue-600 -> text-gray-900 --}}
                 <p>Jalan prof Soedarto</p>
             </div>
             
             {{-- Detail Telepon --}}
             <div class="flex items-center space-x-3 text-lg text-gray-700">
-                <i class="fas fa-phone-alt w-5 h-5 text-blue-600"></i> {{-- ICON TELEPON --}}
+                <i class="fas fa-phone-alt w-5 h-5 text-gray-900"></i> {{-- UBAH: text-blue-600 -> text-gray-900 --}}
                 <p>082185859493</p>
             </div>
             
             {{-- Tombol Google Maps --}}
+            {{-- Ikon di tombol tetap putih karena tombolnya biru, jadi tidak diubah --}}
             <a href="#" class="mt-6 px-6 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 flex items-center space-x-2 transition duration-150 w-max">
-                <i class="fas fa-map-marked-alt"></i> {{-- ICON MAPS (Di Tombol) --}}
+                <i class="fas fa-map-marked-alt"></i>
                 <span>Buka di Google Maps</span>
             </a>
         </div>
