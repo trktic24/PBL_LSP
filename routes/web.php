8502d82d96538b0c9a1249d7aba93ab73ebf9da8
Abyan-11 <kedekkoleng@gmail.com>
2025-10-27 12:18:02 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,9 @@ Route::get('/profil', function () {
 })->name('profil');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/home', [HomeController::class, 'edit'])->name('profile.edit');
-    Route::patch('/home', [HomeController::class, 'update'])->name('profile.update');
-    Route::delete('/home', [HomeController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/home', [HomeController::class, 'edit'])->name('home.edit');
+    Route::patch('/home', [HomeController::class, 'update'])->name('home.update');
+    Route::delete('/home', [HomeController::class, 'destroy'])->name('home.destroy');
 });
 
 require __DIR__.'/auth.php';
