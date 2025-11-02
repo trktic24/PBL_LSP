ee04cc4c400001fdac9301de495c05dff1358e0f
ibnu666-beep <dvibnunabila@gmail.com>
2025-11-02 20:06:10 +0700

    membuat model dan migrasi database form ia.10

---
+++ app/Models/ia10.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ia10 extends Model
+{
+    use HasFactory;
+
+    // Tentukan nama tabel jika tidak jamak
+    protected $table = 'verifikasi_pihak_ketiga';
+
+    // Izinkan semua field ini diisi
+    protected $guarded = [];
+}
