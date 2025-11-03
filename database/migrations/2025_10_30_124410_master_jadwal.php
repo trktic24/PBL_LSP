d29a6a61f39d88fd4975e41f2b44d6f13217d6ab
Bagaskara <bkara7522@gmail.com>
2025-11-03 09:49:44 +0700

    MAPA 02 & penyesuaian data asesi, jadwal, bukti dll -Bagas & Cesar

--- database/migrations/2025_10_30_124410_master_jadwal.php
+++ database/migrations/2025_10_30_124410_master_jadwal.php
@@ -19,9 +19,10 @@ return new class extends Migration
             $table->foreignId('id_tuk')->constrained('master_tuk', 'id_tuk')->onUpdate('cascade')->onDelete('restrict');
             $table->foreignId('id_skema')->constrained('skema', 'id_skema')->onUpdate('cascade')->onDelete('restrict');
             $table->foreignId('id_asesor')->constrained('asesor', 'id_asesor')->onUpdate('cascade')->onDelete('restrict');
-            $table->foreignId('id_asesi')->constrained('asesi', 'id_asesi')->onUpdate('cascade')->onDelete('restrict');
 
             // Isi kolom
+            $table->date('tanggal_mulai');
+            $table->date('tanggal_selesai');
             $table->date('tanggal_pelaksanaan');
             $table->string('Status_jadwal')->comment('Status bisa berupa: Terjadwal, Selesai, Dibatalkan');
             $table->timestamps();
