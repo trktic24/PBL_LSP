ae22af3b56a84cf503c601f6ee29a366a3a01a64
Ghufron Ainun Najib <ghufrnainunajib@gmail.com>
2025-11-03 07:54:42 +0700

    fix error

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -34,9 +34,7 @@
                     </x-slot>
 
                     <x-slot name="content">
-                        <x-dropdown-link :href="route('profil')">
-                            {{ __('profil') }}
-                        </x-dropdown-link>
+
 
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
