b71c49f7a3fa9247254c5cdcf0b8b900473c1c7b
Hamim688 <rajabahamim688@gmail.com>
2025-11-03 08:40:44 +0700

    Feat: Fullstak

---
+++ app/Models/Asesi.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Asesi extends Model
+{
+    use HasFactory;
+
+    // Pastiin ini semua ada
+    protected $table = 'asesi';
+    protected $primaryKey = 'id_asesi';
+
+    // --- INI SOLUSINYA ---
+    /**
+     * Izinkan semua kolom diisi lewat factory/create().
+     */
+    protected $guarded = [];
+    // -----------------------
+
+
+    /**
+     * Relasi ke User
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id_user', 'id_user');
+    }
+
+    /**
+     * Relasi ke DataPekerjaan
+     */
+    public function dataPekerjaan(): HasOne
+    {
+        // Pastiin nama Model-nya bener (DataPekerjaanAsesi)
+        return $this->hasOne(DataPekerjaanAsesi::class, 'id_asesi', 'id_asesi');
+    }
+}
