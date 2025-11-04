ac5d2287ec1b9931cf004ce455da47a35954dd11
Zuuru <zyzull09@gmail.com>
2025-11-04 14:08:39 +0700

    ALLDONE

--- routes/web.php
+++ routes/web.php
@@ -193,4 +193,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require _DIR_.'/auth.php';
+require __DIR__.'/auth.php';
