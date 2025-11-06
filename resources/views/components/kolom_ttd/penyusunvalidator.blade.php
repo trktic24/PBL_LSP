8ee683e72a167fd2829764849ef4b9e620219aa3
ibnu666-beep <dvibnunabila@gmail.com>
2025-11-06 10:37:14 +0700

    edit ttd penyusun validator

--- resources/views/components/kolom_ttd/penyusunvalidator.blade.php
+++ resources/views/components/kolom_ttd/penyusunvalidator.blade.php
@@ -2,43 +2,84 @@
   File: resources/views/components/kolom_ttd/penyusunvalidator.blade.php
   (Ganti seluruh isi file dengan kode ini)
 --}}
-<div class="border border-gray-900 shadow-md">
-    <table class="w-full table-fixed">
-        <thead>
-            <tr class="bg-black text-white">
-                <th class="border border-gray-900 p-2 font-semibold w-[100px]">STATUS</th>
-                <th class="border border-gray-900 p-2 font-semibold w-[200px]">NAMA</th>
-                <th class="border border-gray-900 p-2 font-semibold">NOMOR MET</th>
-                <th class="border border-gray-900 p-2 font-semibold">TANDA TANGAN DAN TANGGAL</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                {{-- Gunakan rowspan="2" agar PENYUSUN mencakup 2 baris --}}
-                <td class="border border-gray-900 p-2 text-sm font-medium bg-black text-white" rowspan="2">PENYUSUN</td>
-                <td class="border border-gray-900 p-2 text-sm"></td>
-                <td class="border border-gray-900 p-2 text-sm"></td>
-                <td class="border border-gray-900 p-2 text-sm h-16"></td>
-            </tr>
-            <tr>
-                {{-- Kolom STATUS tidak perlu diisi lagi karena sudah di-rowspan --}}
-                <td class="border border-gray-900 p-2 text-sm"></td>
-                <td class="border border-gray-900 p-2 text-sm"></td>
-                <td class="border border-gray-900 p-2 text-sm h-16"></td>
-            </tr>
-            <tr>
-                {{-- Gunakan rowspan="2" agar VALIDATOR mencakup 2 baris --}}
-                <td class="border border-gray-900 p-2 text-sm font-medium bg-black text-white" rowspan="2">VALIDATOR</td>
-                <td class="border border-gray-900 p-2 text-sm"></td>
-                <td class="border border-gray-900 p-2 text-sm"></td>
-                <td class="border border-gray-900 p-2 text-sm h-16"></td>
-            </tr>
-            <tr>
-                {{-- Kolom STATUS tidak perlu diisi lagi karena sudah di-rowspan --}}
-                <td class="border border-gray-900 p-2 text-sm"></td>
-                <td class="border border-gray-900 p-2 text-sm"></td>
-                <td class="border border-gray-900 p-2 text-sm h-16"></td>
-            </tr>
-        </tbody>
-    </table>
+
+{{-- 
+  Container utama ini menggantikan <table> lama.
+  Saya menggunakan `space-y-4` untuk memberi jarak antar bagian.
+  Layout ini meniru style dari gambar yang Anda berikan.
+--}}
+<div class="border border-gray-200 shadow-md p-4 rounded-lg bg-white">
+
+    {{-- Judul utama, diambil dari style gambar --}}
+    <h3 class="text-lg font-semibold">
+        Pencatatan dan Validasi
+    </h3>
+
+    {{-- 
+      BAGIAN PENYUSUN
+      Ini menggantikan 2 baris pertama (rowspan="2") dari <tbody> tabel lama.
+      Saya beri jarak 'mb-3' (margin-bottom) antara Penyusun 1 dan 2.
+    --}}
+    <div>
+        <h4 class="font-medium mb-2 text-base">Penyusun</h4>
+        
+        {{-- Data Penyusun 1 --}}
+        <div class="grid grid-cols-[auto_auto_1fr] gap-x-2 gap-y-1 text-sm mb-3">
+            <div>Nama</div>
+            <div>:</div>
+            {{-- 'min-h-[1.5em]' untuk tinggi minimal & border-b untuk garis --}}
+            <div class="min-h-[1.5em]">
+                {{-- Isi data nama penyusun 1 di sini --}}
+            </div>
+
+            <div>No. Reg. MET.</div>
+            <div>:</div>
+            <div class="min-h-[1.5em]">
+                {{-- Isi data no reg 1 di sini --}}
+            </div>
+
+            <div>Tanggal</div>
+            <div>:</div>
+            <div class="min-h-[1.5em]">
+                {{-- Isi data tanggal 1 di sini --}}
+            </div>
+
+            <div>Tanda Tangan</div>
+            <div>:</div>
+            <div class="h-16">
+                {{-- Area Tanda Tangan (seperti h-16 di kode lama) --}}
+            </div>
+        </div>
+
+    {{-- Garis pemisah antar bagian --}}
+    <hr class="my-4">
+
+    {{-- 
+      BAGIAN VALIDATOR
+      Ini menggantikan 2 baris terakhir (rowspan="2") dari <tbody> tabel lama.
+    --}}
+    <div>
+        <h4 class="font-medium mb-2 text-base">Validator</h4>
+        
+        {{-- Data Validator 1 --}}
+        <div class="grid grid-cols-[auto_auto_1fr] gap-x-2 gap-y-1 text-sm mb-3">
+            <div>Nama</div>
+            <div>:</div>
+            <div class="min-h-[1.5em]"></div>
+
+            <div>No. Reg. MET.</div>
+            <div>:</div>
+            <div class="min-h-[1.5em]"></div>
+
+            <div>Tanggal</div>
+            <div>:</div>
+            <div class="min-h-[1.5em]"></div>
+
+            <div>Tanda Tangan</div>
+            <div>:</div>
+            <div class="h-16"></div>
+        </div>
+
+    </div>
+
 </div>
