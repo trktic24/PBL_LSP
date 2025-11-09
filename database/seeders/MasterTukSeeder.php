989cbedf91ddf44768cf5c3488aa1c4799ddaf9f
Dimdim666-trk <adiladimaz2@gmail.com>
2025-11-09 12:58:57 +0700

    Feat: Penyesuaian pada MasterTukSeeder

--- database/seeders/MasterTukSeeder.php
+++ database/seeders/MasterTukSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\MasterTuk;
+use App\Models\MasterTuk; // Menggunakan Model MasterTuk
 
 class MasterTukSeeder extends Seeder
 {
@@ -13,6 +13,12 @@ class MasterTukSeeder extends Seeder
      */
     public function run(): void
     {
+        // 1. MENGHAPUS DATA LAMA (TRUNCATE)
+        // Perintah ini menghapus semua baris data di tabel master_tuk
+        // dan mereset auto-increment ID.
+        MasterTuk::truncate();
+        
+        // 2. MEMBUAT DATA BARU
         // Buat 10 data dummy lokasi TUK
         MasterTuk::factory()->count(10)->create();
     }
