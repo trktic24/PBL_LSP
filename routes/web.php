464ee4000db144e1fba229d94d5ca5def1179521
DimasAdhie12 <bangdims206@gmail.com>
2025-12-02 14:44:08 +0700

    Merubah isi file pada routes web.php

--- routes/web.php
+++ routes/web.php
@@ -72,13 +72,13 @@ Route::get('/FR_AK_02', function () {
     return view('frontend/AK_02/FR_AK_02');
 })->name('FR_AK_02');
 Route::get('/FR_AK_03', function () {
-    return view('frontend/FR_AK_03');
+    return view('frontend/AK_03/FR_AK_03');
 })->name('FR_AK_03');
 Route::get('/FR_AK_04', function () {
     return view('frontend/FR_AK_04');
 })->name('FR_AK_04');
 Route::get('/FR_AK_05', function () {
-    return view('frontend/FR_AK_05');
+    return view('frontend/AK_05/FR_AK_05');
 })->name('FR_AK_05');
 Route::get('/FR_AK_07/{id}', [FrAk07Controller::class, 'create'])->name('fr-ak-07.create');
 
