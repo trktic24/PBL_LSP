6b9a2febae1deaa32a52612cff7b1534afae160b
Diahdwi <diahdwi161003@gmail.com>
2025-12-13 07:22:15 +0700

    gatau ia06

--- app/Models/KunciJawabanIa05.php
+++ app/Models/KunciJawabanIa05.php
@@ -2,26 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class KunciJawabanIa05 extends Model
+class KunciJawabanIA05 extends Model
 {
     use HasFactory;
-
+    
     protected $table = 'kunci_jawaban_ia05';
     protected $primaryKey = 'id_kunci_jawaban_ia05';
-    protected $guarded = [];
 
-    // ================= RELASI =================
-
-    /**
-     * Relasi balik ke Master Soal (One-to-One Inverse).
-     * Kunci jawaban ini MILIK satu soal tertentu.
-     */
-    public function soal()
-    {
-        // belongsTo(RelatedModel, foreign_key_di_tabel_ini, owner_key_di_tabel_induk)
-        return $this->belongsTo(SoalIa05::class, 'id_soal_ia05', 'id_soal_ia05');
-    }
+    protected $fillable = [
+        'id_soal_ia05',
+        'nomor_kunci_jawaban_ia05',
+        'teks_kunci_jawaban_ia05', // <-- Ini kolom kuncinya
+    ];
 }
