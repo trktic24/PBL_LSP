6fa7371bdb536b6664b56391ab716af08cef7e9d
isawich <annisanaelil@gmail.com>
2025-12-15 01:36:28 +0700

    ia08 edit

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,7 @@ use App\Http\Controllers\IA02Controller;
 use App\Http\Controllers\IA05Controller;
 use App\Http\Controllers\Ia06Controller;
 use App\Http\Controllers\IA07Controller;
+use App\Http\Controllers\IA08Controller;
 use App\Http\Controllers\IA09Controller;
 use App\Http\Controllers\IA10Controller;
 use App\Http\Controllers\Ia11Controller;
@@ -180,7 +181,11 @@ Route::post('/asesor/apl02/verifikasi/{id}', [App\Http\Controllers\Asesi\Apl02\P
     Route::post('/FR_IA_07/store', [IA07Controller::class, 'store'])->name('ia07.store');
 
     // IA-08
-    Route::get('/IA_08', fn() => view('frontend/IA_08/IA_08'))->name('IA08');
+    Route::get('/ia08/{id_data_sertifikasi_asesi}', [IA08Controller::class, 'show'])
+     ->name('ia08.show');
+    Route::post('/ia08/store', [IA08Controller::class, 'store'])
+        ->name('ia08.store');
+
 
     // IA-09
     Route::prefix('IA09')->group(function () {
